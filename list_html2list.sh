af472fabbc2459d54a59448c6b6663cc06f499d2
tthtlc <htmldeveloper@gmail.com>
2024-09-25 12:00:09 +0800

    modified:   list_html2list.sh modified:   rotating_spiral.html

--- list_html2list.sh
+++ list_html2list.sh
@@ -1,3 +1,3 @@
 #ls -1tr *.html | sed -e "s/^/'/" -e "s/$/', /"
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v mobile.html | sed -e "s/^/'/" -e "s/$/', /"
+git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v mobile.html | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /"
 #grep -v index.html | grep -v trivia | sed -e "s/^/'/" -e "s/$/', /"
