485a333aba7e9bbeda70346050a901a8eab574ec
Peter Teoh <htmldeveloper@gmail.com>
2024-10-03 22:22:33 +0800

    modified:   index.html modified:   list_html2list.sh modified:   mobile.html modified:   mobile2.html modified:   mobile3.html

--- list_html2list.sh
+++ list_html2list.sh
@@ -1,7 +1,7 @@
 #ls -1tr *.html | sed -e "s/^/'/" -e "s/$/', /"
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile.html | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" 
+git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" 
 
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile.html | grep -v heart_animation.html > /tmp/out$$
+git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html > /tmp/out$$
 
 cat /tmp/out$$ | sed 's/^/+ /' > /tmp/README.md
 
