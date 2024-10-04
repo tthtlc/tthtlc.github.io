46da73dce2b4f5d91f3a147b769b0a8b2b33672d
tthtlc <htmldeveloper@gmail.com>
2024-10-04 13:12:35 +0800

    modified:   find_no_contextmenu.sh

--- find_no_contextmenu.sh
+++ find_no_contextmenu.sh
@@ -1,12 +1,14 @@
 
 ###-e "<script>.*<.script>" 
-grep "document.addEventListener..contextmenu" *.html *.js | sed 's/:.*//g' | sort > /tmp/tmp123$$.1
-ls -1 *.html *.js | sort > /tmp/tmp123$$.2
+grep "document.addEventListener..contextmenu" *.html | sed 's/:.*//g' | sort > /tmp/tmp123$$.1
+ls -1 *.html | sort > /tmp/tmp123$$.2
 list=`diff /tmp/tmp123$$.2 /tmp/tmp123$$.1 | grep "^< " | sed 's/^< //'`
 
 #echo $list
 
 grep -v script.*js.*script $list |grep -v google | sed 's/:.*//' |sort |uniq > /tmp/tmp123$$.3
+
+cat /tmp/tmp123$$.3
 
 grep "script src.*script" `cat /tmp/tmp123$$.3`  > /tmp/tmp123$$.4
 
