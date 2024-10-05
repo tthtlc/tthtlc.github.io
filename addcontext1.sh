494c980fd1e42a46a0acd19ea01e79c8d334dfaa
tthtlc <htmldeveloper@gmail.com>
2024-10-05 10:18:43 +0800

    modified:   addcontext1.sh 	modified:   barnsley_fern_fractal.html 	modified:   bottom.html 	modified:   conway.html 	modified:   diamond2.html 	modified:   diamond6.html 	modified:   dragon_curve.html 	modified:   dragon_curve_animated.html 	modified:   find_no_contextmenu.sh 	modified:   fractal_fern.html 	modified:   hexaflake.html 	modified:   hexagon_rotating_ellipse.html 	modified:   hilbert_curve.html 	modified:   index_spa.html 	modified:   julia_set.html 	modified:   koch_snowflake.html 	modified:   mandelbrot.html 	modified:   mandelbrot_color.html 	modified:   mandelbrot_mouse.html 	modified:   menger_sponge.html 	modified:   mobile2.html 	modified:   mobile3.html 	modified:   pentagon_auto_rotate.html 	modified:   rotating.html 	modified:   rotating_ellipse.html 	modified:   rotating_ellipse2.html 	modified:   rotating_ellipse3.html 	modified:   rotating_ellipse_group.html 	modified:   rotating_ellipse_group_color.html 	modified:   rotating_islamic_spiral.html 	modified:   sierpinski.html 	modified:   sierpinski_carpet.html 	modified:   t_square_fractal2.html 	modified:   t_square_fractal_animation.html 	modified:   vicsek_fractal.html 	modified:   vicsek_fractal_animate.html

--- addcontext1.sh
+++ addcontext1.sh
@@ -1,5 +1,7 @@
+mkdir /tmp/tmp$$
 for name in $*
 do
+	cp $name /tmp/tmp$$
 	mychange '<script>' '<script> \
 document.addEventListener(\"contextmenu\", function(event) { event.preventDefault(); });' $name
 done
--- find_no_contextmenu.sh
+++ find_no_contextmenu.sh
@@ -8,7 +8,14 @@ list=`diff /tmp/tmp123$$.2 /tmp/tmp123$$.1 | grep "^< " | sed 's/^< //'`
 
 grep -v script.*js.*script $list |grep -v google | sed 's/:.*//' |sort |uniq > /tmp/tmp123$$.3
 
-cat /tmp/tmp123$$.3
+grep "script src.*script" `cat /tmp/tmp123$$.3` |grep -v https| sed 's/:.*//' > /tmp/tmp123$$.4
+grep "script src.*script" `cat /tmp/o` |grep -v https| sed 's/:.*//' > /tmp/o1
+diff -Nurp /tmp/tmp123$$.3 /tmp/tmp123$$.4 | grep "< "| sed 's/^< //g'
+
+echo the list above are not tagged with contextmenu.
+
+
+exit
 
 grep "script src.*script" `cat /tmp/tmp123$$.3`  > /tmp/tmp123$$.4
 
