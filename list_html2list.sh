1e6a844fb6b11bc795d673d556934d2a54553818
Peter Teoh <htmldeveloper@gmail.com>
2024-10-05 18:33:06 +0800

    modified:   index.html modified:   list_html2list.sh modified:   zooming_mandelbrot.html

--- list_html2list.sh
+++ list_html2list.sh
@@ -1,10 +1,8 @@
 #ls -1tr *.html | sed -e "s/^/'/" -e "s/$/', /"
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" 
+git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v test | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" 
 
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html > /tmp/out$$
+git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v test | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html > /tmp/out$$
 
 cat /tmp/out$$ | sed 's/^/+ /' > /tmp/README.md
-
-echo "Please replace README.md from /tmp/"
 
 #grep -v index.html | grep -v trivia | sed -e "s/^/'/" -e "s/$/', /"
