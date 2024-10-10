e441b3ae4233759741e08dc25e7bd2810fefe8e3
tthtlc <htmldeveloper@gmail.com>
2024-10-10 13:29:27 +0800

    modified:   README.md deleted:    heart_animation.html modified:   index.html modified:   index_spa.html modified:   list_html2list.sh

--- list_html2list.sh
+++ list_html2list.sh
@@ -1,8 +1,18 @@
 #ls -1tr *.html | sed -e "s/^/'/" -e "s/$/', /"
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v test | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" 
+git ls-files | grep "\.html" | grep -v old | grep -v index | grep -v test | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html | sed -e "s/^/'/" -e "s/$/', /" | tee /tmp/out$$
 
-git ls-files | grep "\.html" | grep -v old | grep -v index.html | grep -v test | grep -v trivia | grep -v backup | grep -v mobile | grep -v heart_animation.html > /tmp/out$$
+mv README.md /tmp/README.md$$
 
-cat /tmp/out$$ | sed 's/^/+ /' > /tmp/README.md
+cat /tmp/out$$ | sed 's/^/+ /' > README.md
+
+mv index.html /tmp/index.html$$
+mv index_spa.html /tmp/index_spa.html$$
+
+./insert_into_template.pl index.tml /tmp/out$$ PETER.TMPL > index.html
+./insert_into_template.pl index_spa.tml /tmp/out$$ PETER.TMPL > index_spa.html
+
+echo "check content of /tmp/out$$"
+
+vi /tmp/out$$
 
 #grep -v index.html | grep -v trivia | sed -e "s/^/'/" -e "s/$/', /"
