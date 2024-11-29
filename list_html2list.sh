445ff5ec9abf43e85c2c57af3249a2aea62c5e0d
tthtlc <htmldeveloper@gmail.com>
2024-11-29 11:20:43 +0800

    modified:   color_gradient_template.html 	modified:   color_gradient_template2.html 	deleted:    comment.html 	modified:   hexagon_chasing_template.html 	modified:   index.html 	modified:   index.tml 	modified:   index_search.html 	modified:   index_search.tml 	modified:   index_spa.html 	modified:   index_spa.tml 	modified:   index_todo.html 	modified:   list_html2list.sh 	deleted:    square3.html 	modified:   square_chasing_template.html 	modified:   triangle_chasing_template.html 	modified:   triangle_chasing_template1.html

--- list_html2list.sh
+++ list_html2list.sh
@@ -8,14 +8,15 @@ cat /tmp/out$$ | sed 's/^\('.*'\)$/+ [\1](\1)/' > README.md
 
 mv index.html /tmp/index.html$$
 mv index_spa.html /tmp/index_spa.html$$
+mv index_all.html /tmp/index_all.html$$
 mv index_search.html /tmp/index_search.html$$
 
 ./list_recenthtml.sh > /tmp/latest$$
 
-./insert_into_template.pl index.tml /tmp/out$$ INDEX.TMPL > /tmp/out$$.1
-./insert_into_template.pl /tmp/out$$.1 /tmp/latest$$ LATEST.TMPL > index.html
+./insert_into_template.pl index.tml /tmp/out$$ INDEX.TMPL > index.html
 ./insert_into_template.pl index_spa.tml /tmp/out$$ INDEX.TMPL > index_spa.html
 ./insert_into_template.pl index_search.tml /tmp/out$$ INDEX.TMPL > index_search.html
+./insert_into_template.pl index_all.tml /tmp/out$$ INDEX.TMPL > index_all.html
 
 echo "check content of /tmp/out$$"
 read data
